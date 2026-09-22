af82b6c7c00c072eea68cd457392c84640648f17
pawmiwow <aichifuluobo@gmail.com>
2026-09-22 18:20:40 +0800

    Adjust overbought and oversold RSI levels

--- forex_rsi_bot.py
+++ forex_rsi_bot.py
@@ -13,8 +13,8 @@ from flask import Flask
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 CHECK_INTERVAL = 300          # 5分钟
 RSI_PERIOD = 14
-OVERBOUGHT = 73
-OVERSOLD = 27
+OVERBOUGHT = 74
+OVERSOLD = 26
 
 PAIRS = [
     "GBPUSD=X", "EURUSD=X", "AUDUSD=X", "NZDUSD=X",
